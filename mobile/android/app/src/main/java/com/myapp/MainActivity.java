6255a1855d79335ea0eeae425bcc51e80ccc8861
Nikola Filipovic <n.filipovic@vegait.rs>
2022-10-14 11:01:26 +0200

    Add mobile navigation setup to the project

--- mobile/android/app/src/main/java/com/myapp/MainActivity.java
+++ mobile/android/app/src/main/java/com/myapp/MainActivity.java
@@ -1,10 +1,20 @@
 package com.myapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  /** This is React Native Navigation prerequisite.
+  Or to quote the documentation:
+  This change is required to avoid crashes related to View state being not persisted consistently across Activity restarts. 
+  */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
