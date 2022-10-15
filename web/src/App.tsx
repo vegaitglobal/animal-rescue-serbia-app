9e38c603bee3a7ac7498a29e18a32edf52b84f0e
bojanvujanac <b.vujanac@vegait.rs>
2022-10-16 01:59:00 +0200

    guard in app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Sidebar from './shared/Sidebar';
 import './scss/style.scss';
+import storageApi from './services/storage.service';
 
 function App() {
+  const navigate = useNavigate();
+
+  if (!storageApi.hasToken()) {
+    navigate('/prijavljivanje');
+  }
+
   return (
     <div className="app">
       <Sidebar />
