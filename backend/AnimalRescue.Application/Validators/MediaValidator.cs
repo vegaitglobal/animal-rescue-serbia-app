89228e1a950a1f4b013b80996651b8e9c3f627f1
Bojan Zelenkovic <bojan.zelenkovic@iwgplc.com>
2022-10-15 21:36:28 +0200

    add media upload validation

---
+++ backend/AnimalRescue.Application/Validators/MediaValidator.cs
@@ -0,0 +1,33 @@
+﻿using AnimalRescue.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace AnimalRescue.Application.Validators
+{
+    public class MediaValidator
+    {
+        private static readonly string[] _allowedExtensions = { ".jpg", ".jpeg", ".png", ".webp", ".webm", ".mp4", ".mov", ".m4v" };
+        // 10 mb max size
+        private static long _maxFileSize = 10 * 1024 * 1024;
+
+        public static void ValidateMedia(IFormFile file)
+        {
+            ValidateFileExtension(Path.GetExtension(file.FileName));
+            ValidateFileSize(file.Length);
+        }
+        private static void ValidateFileExtension(string extension)
+        {
+            if (!_allowedExtensions.Contains(extension.ToLower()))
+            {
+                throw new ValidationException("This file extension is not allowed!");
+            }
+        }
+
+        private static void ValidateFileSize(long fileSize)
+        {
+            if (fileSize > _maxFileSize)
+            {
+                throw new ValidationException($"Maximum allowed file size is {_maxFileSize} bytes.");
+            }
+        }
+    }
+}
