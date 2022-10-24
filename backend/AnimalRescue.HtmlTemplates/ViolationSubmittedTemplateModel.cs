4b01fdb084e8f97de09c5052fd5086bbe7fb4e3f
Milan Bunjevcev <m.bunjevcev@vegait.rs>
2022-10-24 14:18:33 +0200

    fix html template

--- backend/AnimalRescue.HtmlTemplates/ViolationSubmittedTemplateModel.cs
+++ backend/AnimalRescue.HtmlTemplates/ViolationSubmittedTemplateModel.cs
@@ -2,5 +2,9 @@
 
 public class ViolationSubmittedTemplateModel
 {
+    public string Email { get; set; } = default!;
+
     public string Url { get; set; } = default!;
+
+    public string LogoUrl { get; set; } = default!;
 }
