1ea5a38e2201b0b796d857878194335c8faf70d4
Nikola Filipovic <n.filipovic@vegait.rs>
2022-10-31 22:13:54 +0100

    Style violation list to better separate header from main content

--- mobile/src/screens/ViolationsScreen.tsx
+++ mobile/src/screens/ViolationsScreen.tsx
@@ -6,6 +6,7 @@ import {
   SectionListRenderItemInfo,
   SectionListData,
   StyleSheet,
+  TextStyle,
 } from 'react-native';
 import {EmptySpace} from '../components/EmptySpace';
 import {Separator} from '../components/Separator';
@@ -15,6 +16,7 @@ import {loadViolations} from '../store/src/reports/actions';
 import {getViolations} from '../store/src/reports/selectors';
 import {groupBy} from 'lodash';
 import {ScreenRootContainer} from '../components/ScreenRootContainer';
+import {ColorPallet} from '../resources/ColorPallet';
 
 type SectionListViolationGroup = {
   title: string;
@@ -52,7 +54,7 @@ export const ViolationsScreen = () => {
       section: {title},
     }: {
       section: SectionListData<ViolationResponseDto, SectionListViolationGroup>;
-    }) => <Text>{title}</Text>,
+    }) => <Text style={style.sectionHeader}>{title}</Text>,
     [],
   );
 
@@ -64,7 +66,7 @@ export const ViolationsScreen = () => {
         <View style={style.itemRootContainer}>
           <View style={style.titleRowContainer}>
             <Text style={style.addressText}>{address}</Text>
-            <Text>{location}</Text>
+            <Text style={style.locationText}>{location}</Text>
           </View>
           <EmptySpace height={8} />
           {description ? <Text numberOfLines={3}>{description}</Text> : null}
@@ -77,8 +79,10 @@ export const ViolationsScreen = () => {
   return (
     <ScreenRootContainer title="Prekršaji" showLogo>
       <View style={style.rootContainer}>
+        <View style={style.topSpacer} />
         <SectionList
           sections={violationsByGroup}
+          stickySectionHeadersEnabled
           renderItem={renderItem}
           renderSectionHeader={renderSectionHeader}
           SectionSeparatorComponent={() => <EmptySpace height={16} />}
@@ -89,27 +93,53 @@ export const ViolationsScreen = () => {
               <EmptySpace height={8} />
             </>
           )}
-          ListFooterComponent={() => <EmptySpace height={200} />}
+          ListFooterComponent={() => (
+            <View style={style.footerContainer}>
+              <Separator />
+              <EmptySpace height={200} />
+            </View>
+          )}
         />
       </View>
     </ScreenRootContainer>
   );
 };
 
+const titleStyle: TextStyle = {
+  fontWeight: '800',
+  color: ColorPallet.plainBlack,
+};
+
 const style = StyleSheet.create({
   rootContainer: {
-    paddingTop: 16,
     flexGrow: 1,
-    paddingHorizontal: 16,
   },
   itemRootContainer: {
     justifyContent: 'center',
-    paddingHorizontal: 16,
+    paddingHorizontal: 32,
   },
   titleRowContainer: {
     flexDirection: 'row',
   },
   addressText: {
     flex: 1,
+    ...titleStyle,
+  },
+  locationText: {
+    ...titleStyle,
+  },
+  sectionHeader: {
+    backgroundColor: ColorPallet.gray,
+    color: ColorPallet.plainWhite,
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    fontSize: 14,
+  },
+  footerContainer: {
+    transform: [{translateY: -8}],
+  },
+  topSpacer: {
+    height: 24,
+    backgroundColor: ColorPallet.gray,
   },
 });
