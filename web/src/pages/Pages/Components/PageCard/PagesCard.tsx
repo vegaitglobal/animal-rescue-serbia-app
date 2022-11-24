c91bfb847fd0fd61ed8a927c77a70cbacc10ec0c
Stefan Sumar <s.sumar@vegait.rs>
2022-11-24 16:34:52 +0100

    Dodato kreiranje stranice/clanka

--- web/src/pages/Pages/Components/PageCard/PagesCard.tsx
+++ web/src/pages/Pages/Components/PageCard/PagesCard.tsx
@@ -1,12 +1,16 @@
 import { Pencil } from '../../../../shared/Icons';
 import { PagesCardProps } from './PagesCard.data';
 
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 const PagesCard = ({ photoUrl, type, title, description }: PagesCardProps) => {
+  const formatedUrl = BASE_URL + '/' + photoUrl?.replaceAll('\\', '/');
+
   return (
     <div className="cards__card">
       <div
         className="cards__bg has-cover"
-        style={{ backgroundImage: `url(${photoUrl})` }}
+        style={{ backgroundImage: `url(${formatedUrl})` }}
       ></div>
       <div className="cards__content">
         <span className="cards__type">
