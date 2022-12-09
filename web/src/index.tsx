2a0db9898a4bf9f2bd17d7055952201edde5f881
bmarkovicvega <b.markovic@vegait.rs>
2022-12-09 16:57:42 +0100

    Docker prod fix

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import GlobalRouter from './router';
 
 const queryClient = new QueryClient({
@@ -21,8 +20,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
