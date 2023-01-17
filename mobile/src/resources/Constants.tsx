7d6136147ae76ce5953355599e73d9343cee40df
Nikola Filipovic <n.filipovic@vegait.rs>
2023-01-17 12:35:29 +0100

    Add rationale for hardcoded base url constant

--- mobile/src/resources/Constants.tsx
+++ mobile/src/resources/Constants.tsx
@@ -1,4 +1,4 @@
 export const Constants = {
   tokenPersistanceKey: 'accessToken',
-  baseUrl: 'https://api.ars.codeforacause.rs',
+  baseUrl: 'https://api.ars.codeforacause.rs', //TODO: Fix react-native-config library since, currently, it's not fetching env file values consistently
 };
