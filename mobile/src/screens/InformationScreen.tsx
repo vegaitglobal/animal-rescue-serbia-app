cf0cf44d3e452fa15e15a69c8acc76dd94daf151
Nikola Filipovic <n.filipovic@vegait.rs>
2023-03-01 19:38:52 +0100

    Update Information screen text

--- mobile/src/screens/InformationScreen.tsx
+++ mobile/src/screens/InformationScreen.tsx
@@ -16,7 +16,7 @@ export const InformationScreen = () => {
   }, []);
 
   const text =
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!';
+    'Ovde možete pronaći sve važne kontakte za teritoriju opštine na koju izaberete (institucije, službe, veterinari, pet šopovi, organizacije i sl.)';
 
   return (
     <ScreenRootContainer title={'Informisanje'} showLogo>
