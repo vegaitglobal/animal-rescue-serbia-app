aced6eadb12921f4a9c6f902bb5677fe0e440f25
Nikola Filipovic <n.filipovic@vegait.rs>
2023-03-31 12:29:05 +0200

    Fix info screen text

--- mobile/src/screens/InformationScreen.tsx
+++ mobile/src/screens/InformationScreen.tsx
@@ -24,32 +24,8 @@ export const InformationScreen = () => {
   );
 
   const infoText = useMemo(
-    () => `Delatnost službe:\n\n 1. SPASAVANJE\n\n ARS Beograd +381 (0) 64 8 210
-  200\n\n ARS Novi Sad +381 (0) 64 8 210 200\n\n ARS Niš +381 (0) 64 8
-  210 200\n\n ARS Vrčac +381 (0) 64 8 210 200\n\n\n\n 2. LEČENJE,
-  STERILIZACIJA I KASTRACIJA\n\n Beograd:\n\n\n\n JKP „Veterina
-  Beograd”\n Hitna pomoć za životinje:\n\n +381 (0) 62 80 90 340\n +381
-  (0) 62 80 90 333\n www.veterinabeograd.rs\n\n Napomena: \n Besplatna
-  sterilizacija i kastracija za nevlasničke životinje\n\n\n\n
-  Veterinarska ambulanta „Dr Vet”\n +381 (0) 63 23 62 40\n +381 (0) 69
-  74 72 92\n Napomena: \n Besplatna sterilizacija i kastracija za
-  nevlasničke životinje\n\n\n\n Centar za pse i mačke „Jasenak”\n\n +381
-  (0) 34 24 68 452\n\n www.centarzapseimackejasenak.rs\n\n Napomena: \n
-  Jeftinije sterilizacije i kastracije od ostalih u gradu, ali ne
-  besplatno.\n\n\n\n Novi Sad:\n\n\n Prihvatilište „Zoohigijena i
-  Veterina“\n\n +381 (0) 21 64 03 220\n +381 (0) 64 80 31 432\n Email:\n
-  prihvatilistenovisad@gmail.com\n Adresa: Šajkaškog odreda bb\n\n
-  Napomena:\n Besplatna sterilizacija i kastracija za nevlasničke
-  životinje\n\n\n\n 3. PRIHVATILIŠTA:\n\n\n\n Beograd:\n\n\n\n JKP
-  „Veterina Beograd”\n\n +381 (0) 11 17 64 366\n +381 (0) 11 3293099\n
-  +381 (0) 63 54 91 61\n www.veterinabeograd.rs\n\n\n Pomoć
-  životinjama\n www.helpanimals.org.rs\n Napomena: Mačke\n\n Nada za
-  životinje „Riska”\n www.animalhope.org.rs\n\n Napomena: Psi\n\n
-  Prihvatilište „Draževac”\n +381 (0) 65 87 80 177\n
-  www.prihvatilistedrazevac.wordpress.com\n Napomena: Psi i mačke\n\n
-  Azil „Oaza” Avala\n +381 (0) 65 8901 189\n +381 (0) 63 2362 40\n
-  Napomena: Psi\n\n Novi Sad:\n\n Udruženje za zaštitu životinja „S.O.S.
-  For Dogs”\n\n +381 (0) 21 89 788\n\n Napomena: Psi`,
+    () =>
+      'Delatnost službe:\n\n1. SPASAVANJE\n\nARS Beograd +381 (0) 64 8 210 200\n\nARS Novi Sad +381 (0) 64 8 210 200\n\nARS Niš +381 (0) 64 8 210 200\n\nARS Vrčac +381 (0) 64 8 210 200\n\n\n2. LEČENJE, STERILIZACIJA I KASTRACIJA\n\n\nBeograd:\n\n\nJKP „Veterina Beograd”\nHitna pomoć za životinje:\n\n+381 (0) 62 80 90 340\n+381 (0) 62 80 90 333\nwww.veterinabeograd.rs\n\nNapomena: \nBesplatna sterilizacija i kastracija za nevlasničke životinje\n\n\nVeterinarska ambulanta „Dr Vet”\n+381 (0) 63 23 62 40\n+381 (0) 69 74 72 92\nNapomena: \nBesplatna sterilizacija i kastracija za nevlasničke životinje\n\n\nCentar za pse i mačke „Jasenak”\n\n+381 (0) 34 24 68 452\n\nwww.centarzapseimackejasenak.rs\n\nNapomena: \nJeftinije sterilizacije i kastracije od ostalih u gradu, ali ne besplatno.\n\n\nNovi Sad:\n\n\nPrihvatilište „Zoohigijena i Veterina“\n\n+381 (0) 21 64 03 220\n+381 (0) 64 80 31 432\nEmail:\nprihvatilistenovisad@gmail.com\nAdresa: Šajkaškog odreda bb\n\nNapomena:\nBesplatna sterilizacija i kastracija za nevlasničke životinje\n\n\n3. PRIHVATILIŠTA:\n\n\nBeograd:\n\n\nJKP „Veterina Beograd”\n\n+381 (0) 11 17 64 366\n+381 (0) 11 3293099\n+381 (0) 63 54 91 61\nwww.veterinabeograd.rs\n\n\nPomoć životinjama\nwww.helpanimals.org.rs\nNapomena: Mačke\n\nNada za životinje „Riska”\nwww.animalhope.org.rs\n\nNapomena: Psi\n\nPrihvatilište „Draževac”\n+381 (0) 65 87 80 177\nwww.prihvatilistedrazevac.wordpress.com\nNapomena: Psi i mačke\n\nAzil „Oaza” Avala\n+381 (0) 65 8901 189\n+381 (0) 63 2362 40\nNapomena: Psi\n\n\nNovi Sad:\n\n\nUdruženje za zaštitu životinja „S.O.S. For Dogs”\n\n+381 (0) 21 89 788\n\nNapomena: Psi',
     [],
   );
 
@@ -88,7 +64,13 @@ export const InformationScreen = () => {
             </View>
           </View>
 
-          <Text style={{paddingTop: 40}}>{spannableText}</Text>
+          <Text
+            style={{
+              paddingTop: 40,
+              textAlign: 'left',
+            }}>
+            {spannableText}
+          </Text>
         </View>
 
         <EmptySpace height={50} />
